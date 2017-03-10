e10f204263d64272c18ee99ecaf4b4ea65d3d51e
info@norant.be <Boeras23>
2017-03-10 15:40:40 +0100

    login modules toegevoegd

--- FireBase/FinHeartBel/src/app/app.module.ts
+++ FireBase/FinHeartBel/src/app/app.module.ts
@@ -5,14 +5,34 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+import { AngularFireModule} from 'angularfire2';
+import { LoginComponent } from './login/login.component';
+import { EmailComponent } from './email/email.component';
+import { SignupComponent } from './signup/signup.component';
+import { MembersComponent } from './members/members.component';
+
+export const firebaseConfig = {
+apiKey: "AIzaSyCipIk2HHAC0cHIi63D-PbaQocDgHS7mok",
+    authDomain: "finheartbel.firebaseapp.com",
+    databaseURL: "https://finheartbel.firebaseio.com",
+    storageBucket: "finheartbel.appspot.com",
+    messagingSenderId: "1059985304838"
+
+}
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    EmailComponent,
+    SignupComponent,
+    MembersComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]

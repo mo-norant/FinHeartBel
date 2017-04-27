edbfb3d759d0fa35879ebbf373bf6fcfdbb7501d
info@norant.be <Boeras23>
2017-04-27 11:41:12 +0200

    add mockupdata

--- website/src/app/dashboard/dashboard.component.ts
+++ website/src/app/dashboard/dashboard.component.ts
@@ -9,6 +9,8 @@ import { ISlimScrollOptions } from 'ng2-slimscroll';
 export class DashboardComponent implements OnInit {
   opts: ISlimScrollOptions;
   naam: string = 'Dashboard';
+
+  
    public lineChartData:Array<any> = [
     {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
     {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'},

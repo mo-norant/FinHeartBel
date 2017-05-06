83d3c8b0544c62ed9d3f7394ee09faa103557620
mo <mohamedbouzimgroenendaal@mo.home>
2017-05-06 21:23:36 +0200

    integrated OSEA4 java algorithm

--- SpringAPI/src/main/java/com/finbel/MainFinBelAPI.java
+++ SpringAPI/src/main/java/com/finbel/MainFinBelAPI.java
@@ -1,14 +1,44 @@
 package com.finbel;
 
+import java.util.ArrayList;
+import java.util.Random;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class MainFinBelAPI {
+	
+	
+	
+	Random r = new Random();
+	
+	public ArrayList<Integer>  randomgetallen(){
+		ArrayList<Integer> getallenlijst = new ArrayList<Integer>();
+		for(int i = 0 ; i < 10 ; i++){
+		getallenlijst.add(new Integer(r.nextInt(1000)));
+	}
+		return getallenlijst;
+	}
+	
+	
+	
+	
+	
+	
 
 	@RequestMapping("/greeting")
 	public @ResponseBody String greeting(){
 		return "hello" ;
+	}
+	
+	@RequestMapping("/randomgetallen")
+	public @ResponseBody ArrayList<Integer> getallen (HttpServletResponse response){
+		response.setContentType("application/json");
+		
+		return randomgetallen();
 	}
 	
 	

54efed444ba247eca2adde71cea76a1ed2fb72f7
mo laptop <info@norant.be>
2017-05-09 16:58:04 +0200

    verbeterde uploadzone en aanpassing uploadreference

--- website/src/app/uploadzone/uploadzone.component.ts
+++ website/src/app/uploadzone/uploadzone.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import * as firebase from 'firebase';
+
+import { AngularFire, AuthProviders, AuthMethods } from 'angularfire2';
 
 
 @Component({
@@ -8,17 +12,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadzoneComponent implements OnInit {
 
-  
-  constructor() {
+  @Input() percentage: number = 50;
 
-   }
+  constructor(public af: AngularFire) { 
+    this.getECGhistory();
+
+  }
+
 
   ngOnInit() {
   }
 
 
-testupload(event){
+  public getECGhistory() {
+
+    this.af.auth.subscribe(auth => {
+      if (auth) {
 
 
-}
+        var storageRef = firebase.storage().ref();
+
+        var userref = storageRef.child("userstorage/users/" + auth.uid + "/");
+        
+
+
+
+
+
+      }
+    }
+
+    )
+  }
+
+
+
+
+  public fileChangeEvent(fileInput: any) {
+
+
+
+    this.af.auth.subscribe(auth => {
+      if (auth) {
+        var storageRef = firebase.database().ref();
+        var datum = new Date().toString().replace(/ /g,'');
+        console.log(datum);
+        var userref = storageRef.child("userstorage/users/" + auth.uid + "/" + datum + "&&&" + fileInput.target.files[0].name.split('.json').join("")).set(fileInput.target.files[0]);
+
+
+       
+
+        console.log(fileInput.target.files[0]);
+      }
+    });
+
+
+
+  }
 }
